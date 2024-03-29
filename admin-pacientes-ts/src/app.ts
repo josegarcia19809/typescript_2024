8bccc7772306da6dad88183d7f9c8efaa67175ad
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-29 13:06:11 -0600

    Inferencia en Typescript

--- admin-pacientes-ts/src/app.ts
+++ admin-pacientes-ts/src/app.ts
@@ -3,9 +3,9 @@ import { pacienteInput, propietarioInput, emailInput, fechaInput, sintomasInput,
 import { datosCita, submitCita } from './funciones'
 
 // Eventos
-pacienteInput.addEventListener('change', datosCita)
-propietarioInput.addEventListener('change', datosCita)
-emailInput.addEventListener('change', datosCita)
-fechaInput.addEventListener('change', datosCita)
-sintomasInput.addEventListener('change', datosCita)
-formulario.addEventListener('submit', submitCita)
+pacienteInput?.addEventListener('change', datosCita)
+propietarioInput?.addEventListener('change', datosCita)
+emailInput?.addEventListener('change', datosCita)
+fechaInput?.addEventListener('change', datosCita)
+sintomasInput?.addEventListener('change', datosCita)
+formulario?.addEventListener('submit', submitCita)
