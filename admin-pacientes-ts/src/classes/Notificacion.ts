f18bbecec0738aa3a31e0b93618580de7edf1cc4
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-29 13:38:51 -0600

    Creando un Type de Notificacion

--- admin-pacientes-ts/src/classes/Notificacion.ts
+++ admin-pacientes-ts/src/classes/Notificacion.ts
@@ -1,34 +1,54 @@
-import { formulario } from '../selectores'
+import { formulario } from "../selectores";
 
+type NotificacionType = {
+  texto: string;
+  tipo: "error" | "exito" | "";
+};
 export default class Notificacion {
+  notificacion: NotificacionType = {
+    texto : "",
+    tipo : "",
+  };
 
-    constructor({texto, tipo}) {
-        this.texto = texto
-        this.tipo = tipo
-        this.mostrar()
-    }
+  constructor({ texto, tipo }: NotificacionType) {
+    this.notificacion.texto = texto;
+    this.notificacion.tipo = tipo;
+    this.mostrar();
+  }
 
-    mostrar() {
-        // Crear la notificacion
-        const alerta = document.createElement('DIV')
-        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
+  mostrar() {
+    // Crear la notificacion
+    const alerta = document.createElement("DIV");
+    alerta.classList.add(
+      "text-center",
+      "w-full",
+      "p-3",
+      "text-white",
+      "my-5",
+      "alert",
+      "uppercase",
+      "font-bold",
+      "text-sm"
+    );
 
-        // Eliminar alertas duplicadas
-        const alertaPrevia = document.querySelector('.alert')
-        alertaPrevia?.remove()
+    // Eliminar alertas duplicadas
+    const alertaPrevia = document.querySelector(".alert");
+    alertaPrevia?.remove();
 
-        // Si es de tipo error, agrega una clase
-        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500')
+    // Si es de tipo error, agrega una clase
+    this.notificacion.tipo === "error"
+      ? alerta.classList.add("bg-red-500")
+      : alerta.classList.add("bg-green-500");
 
-        // Mensaje de error
-        alerta.textContent = this.texto
+    // Mensaje de error
+    alerta.textContent = this.notificacion.texto;
 
-        // Insertar en el DOM
-        formulario.parentElement.insertBefore(alerta, formulario)
+    // Insertar en el DOM
+    formulario?.parentElement?.insertBefore(alerta, formulario);
 
-        // Quitar después de 5 segundos
-        setTimeout(() => {
-            alerta.remove()
-        }, 3000);
-    }
+    // Quitar después de 5 segundos
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
+  }
 }
