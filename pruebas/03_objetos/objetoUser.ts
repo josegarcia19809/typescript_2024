c0b6471b4d1642c843508d05b72cea4a14336272
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-10 20:27:02 -0600

    Typescript basics: Working with Object Types

---
+++ pruebas/03_objetos/objetoUser.ts
@@ -0,0 +1,15 @@
+let user: {
+    name: string;
+    age: number;
+    isAdmin: boolean;
+    id: string | number
+}
+
+user = {
+    name: "Max",
+    age: 34,
+    isAdmin: true,
+    id: 'abc'
+}
+
+console.log(user);
