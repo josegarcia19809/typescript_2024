1159d06de24c2b09d8096362c2aece24c834c3da
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-10 20:56:43 -0600

    Typescript basics: Defining Function Types

--- pruebas/02_funciones/add_function.ts
+++ pruebas/02_funciones/add_function.ts
@@ -4,3 +4,13 @@ function add(a: number, b: number): number {
 
 
 console.log(add(7, 6))
+
+function calculate(a: number, b: number, calcFn: (a: number, b: number) => number) {
+    console.log(calcFn(a, b));
+}
+
+// El tercer parámetro quye recibe es una función que tiene esta firma:
+// calcFn: (a: number, b: number) => number)
+
+calculate(3, 12, add)
+// add cumple con la firma solicitada
