ddbc52bcb0cd9c1c55643ee0117889e67b8fa202
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-22 19:18:46 -0600

    TypeScript and React Essentials: Defining a Type for Props with "children"

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -3,10 +3,9 @@ import CourseGoal from "./components/CourseGoal.tsx";
 export default function App() {
     return (
         <main>
-            <CourseGoal
-                title="Learn React + TypeScript"
-                description="Learn it from the ground up"
-            />
+            <CourseGoal title="Learn React + TypeScript">
+                <p>Learn it from the ground up</p>
+            </CourseGoal>
         </main>
     )
 }
