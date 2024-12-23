597c42f10409822c0b20fad6d60d38d0048530d5
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-23 10:12:29 -0600

    TypeScript and React Essentials: Handling & Typing Events

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 import goalImg from "./assets/goals.jpg";
 import Header from "./components/Header.tsx";
 import CourseGoalList from "./components/CourseGoalList.tsx";
+import NewGoal from "./components/NewGoal.tsx";
 
 export type CourseGoal = {
     title: string;
@@ -35,7 +36,7 @@ export default function App() {
             <Header image={{src: goalImg, alt: "A list of goals"}}>
                 <h1>Your course goals</h1>
             </Header>
-            <button onClick={handleAddGoal}>Add Goal</button>
+            <NewGoal />
             <CourseGoalList
                 goals={goals}
                 onDeleteGoal={handleDeleteGoal}
