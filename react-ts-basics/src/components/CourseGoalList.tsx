cdbf82d23867afc182823b486a78a4bfb6a4bbbc
josegarcia19809 <josegarcia19809@gmail.com>
2025-01-28 22:01:41 -0600

    Advanced Component Types: Building a More Dynamic & Flexible Component

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,7 +1,8 @@
-import {FC} from "react";
+import {FC, ReactNode} from "react";
 
 import CourseGoal from "./CourseGoal.tsx";
-import {type CourseGoal as CGoal} from "../App.tsx"; // Debido al conflicto de nombres
+import {type CourseGoal as CGoal} from "../App.tsx";
+import InfoBox from "./InfoBox.tsx"; // Debido al conflicto de nombres
 
 interface CourseGoalListProps {
     goals: CGoal[];
@@ -9,8 +10,26 @@ interface CourseGoalListProps {
 }
 
 const CourseGoalList: FC<CourseGoalListProps> = ({goals, onDeleteGoal}) => {
+    if (goals.length === 0) {
+        return (
+            <InfoBox mode="hint">
+                You have no course goals yet. Start adding some!
+            </InfoBox>
+        )
+    }
+
+    let warningBox: ReactNode;
+    if (goals.length >= 4) {
+        warningBox = (
+            <InfoBox mode="warning">
+                You're collecting a lot of goals. Don't put too much on your plate!
+            </InfoBox>
+        )
+    }
+
     return (
         <>
+            {warningBox}
             <ul>
                 {goals.map((goal) => (
                     <li key={goal.id}>
