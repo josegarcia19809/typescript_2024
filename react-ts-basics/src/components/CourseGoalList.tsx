2b722378ccd71706ddcbc4c46b6f80c754659545
josegarcia19809 <josegarcia19809@gmail.com>
2025-01-29 07:51:41 -0600

    Advanced Component Types: Problem. Flexible Components With Required Prop Combinations

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -21,7 +21,7 @@ const CourseGoalList: FC<CourseGoalListProps> = ({goals, onDeleteGoal}) => {
     let warningBox: ReactNode;
     if (goals.length >= 4) {
         warningBox = (
-            <InfoBox mode="warning">
+            <InfoBox mode="warning" severity="high">
                 You're collecting a lot of goals. Don't put too much on your plate!
             </InfoBox>
         )
--- react-ts-basics/src/components/InfoBox.tsx
+++ react-ts-basics/src/components/InfoBox.tsx
@@ -2,10 +2,11 @@ import {type ReactNode} from "react";
 
 type InfoBoxProps = {
     mode: 'hint' | 'warning',
+    severity: 'low' | 'medium' | 'high',
     children: ReactNode,
 }
 
-function InfoBox({mode, children}: InfoBoxProps) {
+function InfoBox({mode, severity, children}: InfoBoxProps) {
     if (mode === 'hint') {
         return (
             <aside className="infobox infobox-hint">
@@ -16,7 +17,7 @@ function InfoBox({mode, children}: InfoBoxProps) {
 
 
     return (
-        <aside className="infobox infobox-warning warning--medium">
+        <aside className={`infobox infobox-warning warning--${severity}`}>
             <h2>Warning</h2>
             <p>{children}</p>
         </aside>
