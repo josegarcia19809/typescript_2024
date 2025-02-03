7614010f3652458e7cf9a71109d0ded6f8f0d5ee
josegarcia19809 <josegarcia19809@gmail.com>
2025-02-03 07:52:46 -0600

    Advanced Component Types: Building Better Wrapper Components with ComponentPropsWithoutRef

--- advanced_components/src/App.tsx
+++ advanced_components/src/App.tsx
@@ -3,8 +3,8 @@ import Input from "./components/Input.tsx";
 function App() {
     return (
         <main>
-            <Input label="Your name:" id="name"/>
-            <Input label="Your age:" id="age"/>
+            <Input label="Your name:" id="name" type="text" />
+            <Input label="Your age:" id="age" type="number" />
         </main>
     );
 }
--- advanced_components/src/components/Input.tsx
+++ advanced_components/src/components/Input.tsx
@@ -1,14 +1,17 @@
+import {ComponentPropsWithoutRef} from "react";
+
 type InputProps = {
     label: string;
     id: string;
-}
+} & ComponentPropsWithoutRef<'input'>
 
-function Input({label, id}: InputProps) {
+
+function Input({label, id, ...props}: InputProps) {
     return (
         <>
             <p>
                 <label htmlFor={id}>{label}</label>
-                <input id={id} type="text"/>
+                <input id={id} type="text" {...props} />
             </p>
         </>
     );
