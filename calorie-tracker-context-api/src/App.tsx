dae4313023e01e9754540ba6b529845af9dbbba3
Jose Garcia <josegarcia19809@gmail.com>
2025-02-23 14:57:51 -0600

    PROYECTO_React_TS - Contador de Calorias con ContextAPI y useReducer: Creando un Hook para la comunicación del Context y Reducer

--- calorie-tracker-context-api/src/App.tsx
+++ calorie-tracker-context-api/src/App.tsx
@@ -1,19 +1,19 @@
-import { useReducer, useEffect, useMemo } from 'react'
+import { useEffect, useMemo} from 'react'
 import Form from "./components/Form"
-import { activityReducer, initialState } from './reducers/activity-reducer'
 import ActivityList from './components/ActivityList'
 import CalorieTracker from './components/CalorieTracker'
+import {useActivity} from "./hooks/useActivity.ts";
 
 function App() {
 
-    const [state, dispatch] = useReducer(activityReducer, initialState)
+    const {state, dispatch} = useActivity();
 
     useEffect(() => {
         localStorage.setItem('activities', JSON.stringify(state.activities))
     }, [state.activities])
 
     const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
-    
+
     return (
         <>
             <header className="bg-lime-600 py-3">
@@ -34,7 +34,7 @@ function App() {
 
             <section className="bg-lime-500 py-20 px-5">
                 <div className="max-w-4xl mx-auto">
-                    <Form 
+                    <Form
                         dispatch={dispatch}
                         state={state}
                     />
@@ -43,14 +43,14 @@ function App() {
 
             <section className='bg-gray-800 py-10'>
                 <div className='max-w-4xl mx-auto'>
-                    <CalorieTracker 
+                    <CalorieTracker
                         activities={state.activities}
                     />
                 </div>
             </section>
 
             <section className="p-10 mx-auto max-w-4xl">
-                <ActivityList 
+                <ActivityList
                     activities={state.activities}
                     dispatch={dispatch}
                 />
