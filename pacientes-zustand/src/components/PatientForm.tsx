0885c7b8b9ecc2e09a9a5b492f625281d7d2e19b
Jose Garcia <josegarcia19809@gmail.com>
2025-03-03 21:30:08 -0600

    Administrador de Pacientes - Zustand: Agregando validación a los otros campos

--- pacientes-zustand/src/components/PatientForm.tsx
+++ pacientes-zustand/src/components/PatientForm.tsx
@@ -59,9 +59,16 @@ export default function PatientForm() {
                         className="w-full p-3  border border-gray-100"
                         type="text"
                         placeholder="Nombre del Propietario"
+                        {...register("caretaker",
+                            {
+                                required: 'El nombre del Propietario es obligatorio',
+                            })
+                        }
                     />
                 </div>
-
+                {errors.caretaker && (
+                    <Error>{errors.caretaker?.message?.toString()}</Error>
+                )}
                 <div className="mb-5">
                     <label htmlFor="email" className="text-sm uppercase font-bold">
                         Email
@@ -71,9 +78,18 @@ export default function PatientForm() {
                         className="w-full p-3  border border-gray-100"
                         type="email"
                         placeholder="Email de Registro"
+                        {...register("email", {
+                            required: "El Email es Obligatorio",
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'Email No Válido'
+                            }
+                        })}
                     />
                 </div>
-
+                {errors.email && (
+                    <Error>{errors.email?.message?.toString()}</Error>
+                )}
                 <div className="mb-5">
                     <label htmlFor="date" className="text-sm uppercase font-bold">
                         Fecha Alta
@@ -82,9 +98,16 @@ export default function PatientForm() {
                         id="date"
                         className="w-full p-3  border border-gray-100"
                         type="date"
+                        {...register("date",
+                            {
+                                required: 'La fecha es obligatoria',
+                            })
+                        }
                     />
                 </div>
-
+                {errors.date && (
+                    <Error>{errors.date?.message?.toString()}</Error>
+                )}
                 <div className="mb-5">
                     <label htmlFor="symptoms" className="text-sm uppercase font-bold">
                         Síntomas
@@ -93,9 +116,16 @@ export default function PatientForm() {
                         id="symptoms"
                         className="w-full p-3  border border-gray-100"
                         placeholder="Síntomas del paciente"
-                    ></textarea>
+                        {...register("symptoms",
+                            {
+                                required: 'Los sintomas son obligatorios',
+                            })
+                        }
+                    />
                 </div>
-
+                {errors.symptoms && (
+                    <Error>{errors.symptoms?.message?.toString()}</Error>
+                )}
                 <input
                     type="submit"
                     className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
