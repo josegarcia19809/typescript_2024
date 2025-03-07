ea2e15b69c19ade240262c5a43371f031cd58152
Jose Garcia <josegarcia19809@gmail.com>
2025-03-07 07:47:50 -0600

    Administrador de Pacientes-Zustand: Eliminar Pacientes en Zustand

--- pacientes-zustand/src/components/PatientsDetails.tsx
+++ pacientes-zustand/src/components/PatientsDetails.tsx
@@ -1,11 +1,14 @@
 import {Patient} from "../types";
 import PatientDetailItem from "./PatientDetailItem.tsx";
+import {usePatientStore} from "../store/store.ts";
 
 type PatientsDetailsProps = {
     patient: Patient;
 }
 
 function PatientsDetails({patient}: PatientsDetailsProps) {
+
+    const deletePatient = usePatientStore(state => state.deletePatient)
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
             <PatientDetailItem label="Id" data={patient.id}/>
@@ -17,15 +20,17 @@ function PatientsDetails({patient}: PatientsDetailsProps) {
 
             <div className="flex justify-between mt-10">
                 <button
-                type="button"
-                className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold
                 uppercase rounded-lg">
                     Editar
                 </button>
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold
-                uppercase rounded-lg">
+                uppercase rounded-lg"
+                onClick={() => deletePatient(patient.id)}
+                >
                     Eliminar
                 </button>
             </div>
