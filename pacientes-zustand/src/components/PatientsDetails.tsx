dc722280f195b8695fbb44b3982518cac5b73b7b
Jose Garcia <josegarcia19809@gmail.com>
2025-03-07 08:24:34 -0600

    Administrador de Pacientes-Zustand: Obtener el paciente a editar

--- pacientes-zustand/src/components/PatientsDetails.tsx
+++ pacientes-zustand/src/components/PatientsDetails.tsx
@@ -9,6 +9,7 @@ type PatientsDetailsProps = {
 function PatientsDetails({patient}: PatientsDetailsProps) {
 
     const deletePatient = usePatientStore(state => state.deletePatient)
+    const getPatientById = usePatientStore(state => state.getPatientById)
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
             <PatientDetailItem label="Id" data={patient.id}/>
@@ -21,15 +22,19 @@ function PatientsDetails({patient}: PatientsDetailsProps) {
             <div className="flex justify-between mt-10">
                 <button
                     type="button"
-                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold
-                uppercase rounded-lg">
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white
+                                font-bold uppercase rounded-lg"
+                    onClick={() => {
+                        getPatientById(patient.id)
+                    }}
+                >
                     Editar
                 </button>
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold
-                uppercase rounded-lg"
-                onClick={() => deletePatient(patient.id)}
+                                uppercase rounded-lg"
+                    onClick={() => deletePatient(patient.id)}
                 >
                     Eliminar
                 </button>
