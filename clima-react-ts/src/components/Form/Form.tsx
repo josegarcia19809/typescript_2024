398751100f9a88ee865adf274dcc7ca64a6982a5
Jose Garcia <josegarcia19809@gmail.com>
2025-03-27 05:54:53 -0600

    Proyecto ReactTS. Clima API: Colocando en el State la ciudad y pais a consultar el Clima

--- clima-react-ts/src/components/Form/Form.tsx
+++ clima-react-ts/src/components/Form/Form.tsx
@@ -1,7 +1,23 @@
 import {countries} from "../../data/countries.ts";
 import styles from "./Form.module.css";
+import {ChangeEvent, useState} from "react";
+import {SearchType} from "../../types/index.ts";
+
 
 function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: '',
+    });
+
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value,
+        })
+    };
     return (
         <>
             <form className={styles.form}>
@@ -12,12 +28,19 @@ function Form() {
                         name="city"
                         type="text"
                         placeholder="Ciudad"
+                        value={search.city}
+                        onChange={handleChange}
                     />
                 </div>
 
                 <div className={styles.field}>
-                    <label>Pais</label>
-                    <select>
+                    <label htmlFor="country">Pais</label>
+                    <select
+                        id="country"
+                        value={search.country}
+                        onChange={handleChange}
+                        name="country"
+                    >
                         <option value="">-- Seleccione un pais --</option>
                         {countries.map(country => (
                             <option
