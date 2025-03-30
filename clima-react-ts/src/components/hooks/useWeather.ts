1cabef00a0bc1876fbabd71f381660f9b1de4271
Jose Garcia <josegarcia19809@gmail.com>
2025-03-30 17:14:31 -0600

    Proyecto ReactTS. Clima API: Tipar Resultados de una API - Validar con ZOD

--- clima-react-ts/src/components/hooks/useWeather.ts
+++ clima-react-ts/src/components/hooks/useWeather.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
-import {SearchType, Weather} from "../../types/index.ts";
+import {SearchType} from "../../types/index.ts";
+import {z} from "zod";
+
+
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number()
+    })
+})
+type Weather = z.infer<typeof Weather>
 
 function UseWeather() {
 
@@ -13,9 +25,14 @@ function UseWeather() {
             const lon = data[0].lon;
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
-            const {data: weatherData} = await axios<Weather>(weatherUrl);
-            console.log(weatherData.main.temp);
-            console.log(weatherData.name);
+            const {data: weatherData} = await axios(weatherUrl);
+            const result = Weather.safeParse(weatherData);
+            if (result.success) {
+                console.log(result.data.name);
+                console.log(result.data.main.temp);
+            }
+            // console.log(weatherData.main.temp);
+            // console.log(weatherData.name);
 
         } catch (err) {
             console.log(err)
