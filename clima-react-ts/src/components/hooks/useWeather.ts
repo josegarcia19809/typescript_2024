4d01ee0945edfcb91507ec2333e51bec37c0e70a
Jose Garcia <josegarcia19809@gmail.com>
2025-03-30 07:49:08 -0600

    Proyecto ReactTS. Clima API: Segundo llamado hacia la API para obtener el Clima

--- clima-react-ts/src/components/hooks/useWeather.ts
+++ clima-react-ts/src/components/hooks/useWeather.ts
@@ -8,7 +8,14 @@ function UseWeather() {
             const appId = import.meta.env.VITE_API_KEY;
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
             const {data} = await axios.get(geoUrl);
-            console.log(data);
+
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+            const {data: weatherData} = await axios.get(weatherUrl);
+            console.log(weatherData);
+
         } catch (err) {
             console.log(err)
         }
