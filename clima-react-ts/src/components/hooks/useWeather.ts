6d90afdf81b1f595d2c8b0716a4a4b8c17c52e73
Jose Garcia <josegarcia19809@gmail.com>
2025-03-30 07:39:08 -0600

    Proyecto ReactTS. Clima API: variables de Entorno

--- clima-react-ts/src/components/hooks/useWeather.ts
+++ clima-react-ts/src/components/hooks/useWeather.ts
@@ -5,7 +5,7 @@ function UseWeather() {
 
     const fetchWeather = async (search: SearchType) => {
         try {
-            const appId = '';
+            const appId = import.meta.env.VITE_API_KEY;
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
             const {data} = await axios.get(geoUrl);
             console.log(data);
