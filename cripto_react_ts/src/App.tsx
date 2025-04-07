5be8d4b2de9c5c84cf1d0d0cf8d9c14bfd3bb601
Jose Garcia <josegarcia19809@gmail.com>
2025-04-06 18:31:04 -0600

    Cotizador de Criptomonedas, React TS: Primeros pasos con código CSS

--- cripto_react_ts/src/App.tsx
+++ cripto_react_ts/src/App.tsx
@@ -1,6 +1,16 @@
 function App() {
     return (
-        <></>
+        <>
+            <div className="container">
+                <h1 className="app-title">
+                    Cotizador de <span>Criptomonedas</span>
+                </h1>
+
+                <div className="content">
+
+                </div>
+            </div>
+        </>
     );
 }
 
