1af83c10ea2c14d79130dfb299cd82191116011a
Jose Garcia <josegarcia19809@gmail.com>
2025-04-13 08:10:13 -0600

    Cotizador de Criptomonedas, React TS: Validando la respuesta de la API con Zod

--- cripto_react_ts/src/store.ts
+++ cripto_react_ts/src/store.ts
@@ -1,11 +1,15 @@
 import {create} from "zustand";
 import axios from 'axios';
+import {
+    CryptoCurrenciesResponseSchema
+} from "./schema/crypto-schema.ts";
 
 
 async function getCryptos() {
     const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
     const {data: {Data}} = await axios(url);
-    console.log(Data);
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+    console.log(result);
 }
 
 
