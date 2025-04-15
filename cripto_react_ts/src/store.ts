d49ff34e193b91a662c94ca8b2711a59214c332d
Jose Garcia <josegarcia19809@gmail.com>
2025-04-15 16:35:56 -0600

    Cotizador de Criptomonedas, React TS: Moviendo la función que obtiene la API hacia otro archivo

--- cripto_react_ts/src/store.ts
+++ cripto_react_ts/src/store.ts
@@ -1,23 +1,11 @@
 import {create} from "zustand";
-import axios from 'axios';
-import {
-    CryptoCurrenciesResponseSchema
-} from "./schema/crypto-schema.ts";
+import {getCryptos} from "./services/CryptoService.ts";
 import {Cryptocurrency} from "./types";
 import {devtools} from 'zustand/middleware';
 
 type CryptoStore = {
     cryptoscurrencies: Cryptocurrency[]
     fetchCryptos: () => Promise<void>
-}
-
-async function getCryptos() {
-    const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
-    const {data: {Data}} = await axios(url);
-    const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-    if (result.success) {
-        return result.data;
-    }
 }
 
 
