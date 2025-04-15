ae6c826c197cc734bdb30b5516a30c4dc3a5391b
Jose Garcia <josegarcia19809@gmail.com>
2025-04-15 16:26:56 -0600

    Cotizador de Criptomonedas, React TS: Finalizando la función que obtiene las Criptos

--- cripto_react_ts/src/store.ts
+++ cripto_react_ts/src/store.ts
@@ -3,18 +3,29 @@ import axios from 'axios';
 import {
     CryptoCurrenciesResponseSchema
 } from "./schema/crypto-schema.ts";
+import {Cryptocurrency} from "./types";
 
+type CryptoStore = {
+    cryptoscurrencies: Cryptocurrency[]
+    fetchCryptos: () => Promise<void>
+}
 
 async function getCryptos() {
     const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
     const {data: {Data}} = await axios(url);
     const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-    console.log(result);
+    if (result.success) {
+        return result.data;
+    }
 }
 
 
-export const useCryptoStore = create(() => ({
-    fetchCryptos: () => {
-        getCryptos();
+export const useCryptoStore = create<CryptoStore>((set) => ({
+    cryptoscurrencies: [],
+    fetchCryptos: async () => {
+        const cryptocurrencies = await getCryptos();
+        set(() => ({
+            cryptoscurrencies: cryptocurrencies,
+        }))
     }
 }));
