bf04e5925b6e0ba15319c06cbf7eec353193031e
Jose Garcia <josegarcia19809@gmail.com>
2025-04-16 09:14:45 -0600

    Cotizador de Criptomonedas, React TS: Validando la Respuesta de la API con ZOD

--- cripto_react_ts/src/services/CryptoService.ts
+++ cripto_react_ts/src/services/CryptoService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {CryptoCurrenciesResponseSchema} from "../schema/crypto-schema.ts";
+import {CryptoCurrenciesResponseSchema, CryptoPriceSchema} from "../schema/crypto-schema.ts";
 import {Pair} from "../types";
 
 export async function getCryptos() {
@@ -14,5 +14,8 @@ export async function getCryptos() {
 export async function fetchCurrentCryptoPrice(pair: Pair) {
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.criptocurrency}&tsyms=${pair.currency}`;
     const {data: {DISPLAY}} = await axios(url);
-    console.log(DISPLAY[pair.criptocurrency][pair.currency]);
+    const result = CryptoPriceSchema.safeParse(DISPLAY[pair.criptocurrency][pair.currency]);
+    if (result.success) {
+        return result.data;
+    }
 }
