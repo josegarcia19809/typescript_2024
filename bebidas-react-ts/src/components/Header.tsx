991055c7b0ba55a493454ac816acc9731bc31948
Jose Garcia <josegarcia19809@gmail.com>
2025-05-05 06:21:24 -0600

    TS React. Bebidas y Recetas Router DOM: El Hook useLocation() para saber información de la ruta actual

--- bebidas-react-ts/src/components/Header.tsx
+++ bebidas-react-ts/src/components/Header.tsx
@@ -1,5 +1,11 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
+
 function Header() {
+
+    const {pathname} = useLocation();
+
+    console.log(pathname)
+
     return (
         <header className="bg-slate-800">
             <div className="mx-auto container px-5 py-16">
@@ -10,11 +16,11 @@ function Header() {
 
                     <nav className="flex gap-4">
                         <NavLink to="/" className={
-                            ({isActive})=>
+                            ({isActive}) =>
                                 isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
                         }>Inicio</NavLink>
                         <NavLink to="/favoritos" className={
-                            ({isActive})=>
+                            ({isActive}) =>
                                 isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
                         }>Favoritos</NavLink>
                     </nav>
