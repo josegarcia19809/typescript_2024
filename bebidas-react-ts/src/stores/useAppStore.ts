927bba2ed349c2a10e376ba57d1d1201ac843456
Jose Garcia <josegarcia19809@gmail.com>
2025-05-17 14:54:00 -0600

    TS React. Bebidas y Recetas Router DOM: Colocar las categorías en el State

--- bebidas-react-ts/src/stores/useAppStore.ts
+++ bebidas-react-ts/src/stores/useAppStore.ts
@@ -1,7 +1,8 @@
 import {create} from "zustand";
+import {devtools} from "zustand/middleware";
 import {createRecipeSlice, RecipesSlicesType} from "./recipeSlice.ts";
 
 
-export const useAppStore = create<RecipesSlicesType>((...a) => ({
+export const useAppStore = create<RecipesSlicesType>()(devtools((...a) => ({
     ...createRecipeSlice(...a)
-}))
+})))
