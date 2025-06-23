9152b41b800b1aa72e6aac764dc5dc288631fbb2
Jose Garcia <josegarcia19809@gmail.com>
2025-06-23 08:29:18 -0600

    TS React. Bebidas y Recetas Router DOM: Creando un Slice para favoritos

--- bebidas-react-ts/src/stores/useAppStore.ts
+++ bebidas-react-ts/src/stores/useAppStore.ts
@@ -1,8 +1,10 @@
 import {create} from "zustand";
 import {devtools} from "zustand/middleware";
 import {createRecipeSlice, RecipesSlicesType} from "./recipeSlice.ts";
+import {FavoritesSliceType, createFavoritesSlice} from "./favoritesSlice.ts";
 
 
-export const useAppStore = create<RecipesSlicesType>()(devtools((...a) => ({
-    ...createRecipeSlice(...a)
+export const useAppStore = create<RecipesSlicesType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a),
 })))
