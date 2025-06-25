fcbe1397130efae5a0e3c657db5dd884eea86421
Jose Garcia <josegarcia19809@gmail.com>
2025-06-25 12:35:32 -0600

    TS React. Bebidas y Recetas Router DOM: Agregar Favoritos a LocalStorage

--- bebidas-react-ts/src/layouts/Layout.tsx
+++ bebidas-react-ts/src/layouts/Layout.tsx
@@ -1,8 +1,15 @@
 import {Outlet} from "react-router-dom";
 import Header from "../components/Header.tsx";
 import Modal from "../components/Modal.tsx";
+import {useAppStore} from "../stores/useAppStore.ts";
+import {useEffect} from "react";
 
 function Layout() {
+
+    const loadFromLocalStorage = useAppStore((state) => state.loadFromLocalStorage);
+    useEffect(() => {
+        loadFromLocalStorage()
+    }, []);
     return (
         <>
             <Header/>
