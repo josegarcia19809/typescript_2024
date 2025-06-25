949adab7b3e80336b8d92ea98b2c43b8454508a5
Jose Garcia <josegarcia19809@gmail.com>
2025-06-25 12:57:12 -0600

    TS React. Bebidas y Recetas Router DOM: Creando un Slice para Notificaciones

--- bebidas-react-ts/src/stores/useAppStore.ts
+++ bebidas-react-ts/src/stores/useAppStore.ts
@@ -2,9 +2,11 @@ import {create} from "zustand";
 import {devtools} from "zustand/middleware";
 import {createRecipeSlice, RecipesSlicesType} from "./recipeSlice.ts";
 import {FavoritesSliceType, createFavoritesSlice} from "./favoritesSlice.ts";
+import {NotificationSliceType, createNotificationSlice} from "./notificationSlice.ts";
 
 
-export const useAppStore = create<RecipesSlicesType & FavoritesSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSlicesType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
     ...createRecipeSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
 })))
