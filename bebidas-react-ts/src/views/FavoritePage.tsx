8f4c75c69ac8b0ed5b7d68afa6443d0d1c7fdd35
Jose Garcia <josegarcia19809@gmail.com>
2025-06-25 12:47:01 -0600

    TS React. Bebidas y Recetas Router DOM: Primeros pasos con la página de favoritos

--- bebidas-react-ts/src/views/FavoritePage.tsx
+++ bebidas-react-ts/src/views/FavoritePage.tsx
@@ -1,7 +1,26 @@
+import {useAppStore} from "../stores/useAppStore.ts";
+import DrinkCard from "../components/DrinkCard.tsx";
+import {useMemo} from "react";
+
 function FavoritePage() {
+
+    const favorites = useAppStore((state) => state.favorites);
+    const hasFavorites = useMemo(() => favorites.length, [favorites]);
+
     return (
         <>
-        <h1>Favoritos</h1>
+            <h1 className="text-6xl font-extrabold">Favoritos</h1>
+            {hasFavorites ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+                    {favorites.map((drink) => (
+                        <DrinkCard
+                            drink={drink}
+                            key={drink.idDrink}/>
+                    ))}
+                </div>
+            ):(
+                <p className="my-10 text-center text-2xl">Los favoritos se mostrarán aquí...</p>
+            )}
         </>
     );
 }
