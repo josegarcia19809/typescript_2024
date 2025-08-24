fcc968cd98444416078e6d34ea032f3a0fab184d
Jose Garcia <josegarcia19809@gmail.com>
2025-08-24 13:35:11 -0600

    TS. React: Hola Mundo

---
+++ hola_mundo/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <h1>Usando REACT</h1>
+            <h2>Presentando JSX</h2>
+            <img src='/img/heroe.jpg' alt=""/>
+
+        </div>
+    )
+}
+
+export default App
