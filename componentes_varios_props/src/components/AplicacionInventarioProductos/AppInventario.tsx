e6a8a0976bdbf5d0ed0875f461ee3ff15f63b495
Jose Garcia <josegarcia19809@gmail.com>
2025-09-30 19:41:47 -0600

    Ts React: Componentes con varios props. Inventario productos

--- componentes_varios_props/src/components/AplicacionInventarioProductos/AppInventario.tsx
+++ componentes_varios_props/src/components/AplicacionInventarioProductos/AppInventario.tsx
@@ -1,15 +1,14 @@
 import Producto from "./Producto"
 
 const AppInventario = () => {
-  return (
-      <div>
-          <h1>🍿💻Inventario de productos</h1>
-          <Producto stock={50} nombre="Laptop" disponible={true} />
-          <Producto stock={12} nombre="Teclado mecánico" disponible={true} />
-          <Producto stock={0} nombre="Mouse inalámbrico" disponible={false} />
-          
-    </div>
-  )
+    return (
+        <div>
+            <h1>💻Inventario de productos</h1>
+            <Producto stock={50} nombre="Laptop 💻" disponible={true}/>
+            <Producto stock={12} nombre="Teclado mecánico ⌨️" disponible={true}/>
+            <Producto stock={0} nombre="Mouse inalámbrico 🖱️" disponible={false}/>
+        </div>
+    )
 }
 
 export default AppInventario
--- componentes_varios_props/src/components/AplicacionInventarioProductos/Producto.tsx
+++ componentes_varios_props/src/components/AplicacionInventarioProductos/Producto.tsx
@@ -4,18 +4,18 @@ type ProductoProps = {
     disponible: boolean;
 }
 
-const Producto = ({stock, nombre, disponible }: ProductoProps) => {
-  return (
-      <div style={{
-          border: "1px solid gray",
-          padding: "10px",
-          margin: "10px"
-      }}>
-          <h2>Producto: {nombre}</h2> 
-          <p>Cantidad en stock: {stock}</p>
-          <p>{ disponible ? "✅Está disponible": "❌No está disponible"}</p>
-    </div>
-  )
+const Producto = ({stock, nombre, disponible}: ProductoProps) => {
+    return (
+        <div style={{
+            border: "1px solid gray",
+            padding: "10px",
+            margin: "10px"
+        }}>
+            <h2>Producto: {nombre}</h2>
+            <p>Cantidad en stock: {stock}</p>
+            <p>{disponible ? "✅Está disponible" : "❌No está disponible"}</p>
+        </div>
+    )
 }
 
 export default Producto
