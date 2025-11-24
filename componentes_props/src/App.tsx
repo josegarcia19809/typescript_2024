59f9c9a8700fde8e851ed35b3aee30bee5511479
Jose Garcia <josegarcia19809@gmail.com>
2025-11-24 14:15:02 -0600

    MeRge

--- componentes_props/src/App.tsx
+++ componentes_props/src/App.tsx
@@ -1,19 +1,10 @@
 // App.tsx
 
-<<<<<<< Updated upstream
-import TorneoApp from "./components/AplicacionRegistroGoles/TorneoApp.tsx";
-=======
-import AlbumsList from "./components/AlbumsList.tsx";
->>>>>>> Stashed changes
 
 const App = () => {
     return (
         <>
-<<<<<<< Updated upstream
-            <TorneoApp />
-=======
-            <AlbumsList />
->>>>>>> Stashed changes
+
         </>
     );
 };
