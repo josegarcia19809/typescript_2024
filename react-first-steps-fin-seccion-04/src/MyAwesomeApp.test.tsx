73049decd8025e740f219bd4e646aa0cedea021d
Jose Garcia <josegarcia19809@gmail.com>
2025-12-16 08:21:14 -0600

    Pruebas automáticas - Unit testing: Buscar elementos en el componente renderizado

--- react-first-steps-fin-seccion-04/src/MyAwesomeApp.test.tsx
+++ react-first-steps-fin-seccion-04/src/MyAwesomeApp.test.tsx
@@ -1,10 +1,25 @@
-import {describe, test} from "vitest";
+import {describe, expect, test} from "vitest";
 import {MyAwesomeApp} from "./MyAwesomeApp.tsx";
 import {render, screen} from "@testing-library/react";
 
 describe("MyAwesomeApp", () => {
     test("Should render firstName and lastName", async () => {
-        render(<MyAwesomeApp />);
-        screen.debug()
+        const {container} = render(<MyAwesomeApp/>);
+        const h1 = container.querySelector("h1");
+        const h3 = container.querySelector("h3");
+        console.log("-".repeat(100))
+        expect(h1?.innerHTML).toContain("Fernando");
+        expect(h3?.innerHTML).toContain("Herrera");
+    })
+
+    test("Should render firstName and lastName - screen", async () => {
+        render(<MyAwesomeApp/>);
+        screen.debug();
+        console.log("-".repeat(100))
+        const h1 = screen.getByTestId("first-name-title");
+        console.log(h1?.innerHTML);
+
+        expect(h1?.innerHTML).toContain("Fernando");
     })
 })
+
