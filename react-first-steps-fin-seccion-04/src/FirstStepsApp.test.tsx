21a55c896b5c71d1acaa335bf19ea630ed53a3ab
Jose Garcia <josegarcia19809@gmail.com>
2025-12-16 13:43:55 -0600

    Pruebas automáticas - Unit testing: Esperar argumentos específicos

--- react-first-steps-fin-seccion-04/src/FirstStepsApp.test.tsx
+++ react-first-steps-fin-seccion-04/src/FirstStepsApp.test.tsx
@@ -1,12 +1,25 @@
-import {describe, expect, test, vi} from "vitest";
+import {afterEach,describe, expect, test, vi} from "vitest";
 import {render, screen} from "@testing-library/react";
 import {FirstStepsApp} from "./FirstStepsApp.tsx";
 
-vi.mock("./shopping-cart/ItemCounter", () => ({
-    ItemCounter: () => <div data-testid="ItemCounter"></div>
+// vi.mock("./shopping-cart/ItemCounter", () => ({
+//     ItemCounter: () => <div data-testid="ItemCounter"></div>
+// }));
+
+const mockItemCounter = vi.fn((_props: unknown) => {
+    return <div data-testid="ItemCounter" />;
+});
+
+vi.mock('./shopping-cart/ItemCounter', () => ({
+    ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
 
 describe("FirstStepsApp", () => {
+
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
     test("Should match snapshot", () => {
         const {container} = render(<FirstStepsApp/>);
 
@@ -19,4 +32,22 @@ describe("FirstStepsApp", () => {
         expect(itemCounters.length).toBe(3);
 
     })
+
+    test('should render ItemCounter with correct props', () => {
+        render(<FirstStepsApp />);
+
+        expect(mockItemCounter).toHaveBeenCalledTimes(3);
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Nintendo Switch 2',
+            quantity: 1,
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Pro Controller',
+            quantity: 2,
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Super Smash',
+            quantity: 5,
+        });
+    });
 })
