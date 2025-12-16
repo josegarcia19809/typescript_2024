cdc56a4dce523a97998336b6979ad1be0621074d
Jose Garcia <josegarcia19809@gmail.com>
2025-12-16 12:16:14 -0600

    Pruebas automáticas - Unit testing: Disparar eventos

--- react-first-steps-fin-seccion-04/src/shopping-cart/ItemCounter.test.tsx
+++ react-first-steps-fin-seccion-04/src/shopping-cart/ItemCounter.test.tsx
@@ -1,5 +1,5 @@
 import {describe, expect, test} from "vitest";
-import {render, screen} from "@testing-library/react";
+import {render, screen, fireEvent} from "@testing-library/react";
 import {ItemCounter} from "./ItemCounter.tsx";
 
 describe("ItemCounter", () => {
@@ -22,4 +22,51 @@ describe("ItemCounter", () => {
 
         expect(screen.getByText(quantity)).toBeDefined();
     })
+
+    test("Should increase count when +1 button is pressed", () => {
+
+        render(<ItemCounter name={"Test item"} quantity={1}/>);
+
+        const [buttonAdd] = screen.getAllByRole("button");
+
+
+        screen.debug();
+        console.log("-".repeat(100));
+
+        fireEvent.click(buttonAdd);
+        expect(screen.getByText('2')).toBeDefined();
+
+    })
+
+    test("Should decrease count when -1 button is pressed", () => {
+
+        render(<ItemCounter name={"Test item"} quantity={5}/>);
+
+        const [, buttonSubstract] = screen.getAllByRole("button");
+
+
+        screen.debug();
+        console.log("-".repeat(100));
+
+        fireEvent.click(buttonSubstract);
+        expect(screen.getByText('4')).toBeDefined();
+
+    })
+
+    test("Should not decrease count when -1 button is pressed and quatity = 1", () => {
+
+        render(<ItemCounter name={"Test item"} quantity={1}/>);
+
+        const [, buttonSubstract] = screen.getAllByRole("button");
+
+
+        screen.debug();
+        console.log("-".repeat(100));
+
+        fireEvent.click(buttonSubstract);
+        expect(screen.getByText('1')).toBeDefined();
+
+    })
+
+
 })
