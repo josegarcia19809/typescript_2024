b13cdd97c31e378b02fb747732106903a80df298
Jose Garcia <josegarcia19809@gmail.com>
2025-12-16 13:23:15 -0600

    Pruebas automáticas - Unit testing: Componentes fictícios - Mock Components

--- react-first-steps-fin-seccion-04/src/FirstStepsApp.test.tsx
+++ react-first-steps-fin-seccion-04/src/FirstStepsApp.test.tsx
@@ -1,11 +1,22 @@
-import {describe, expect, test} from "vitest";
+import {describe, expect, test, vi} from "vitest";
 import {render, screen} from "@testing-library/react";
 import {FirstStepsApp} from "./FirstStepsApp.tsx";
+
+vi.mock("./shopping-cart/ItemCounter", () => ({
+    ItemCounter: () => <div data-testid="ItemCounter"></div>
+}));
 
 describe("FirstStepsApp", () => {
     test("Should match snapshot", () => {
         const {container} = render(<FirstStepsApp/>);
 
         expect(container).toMatchSnapshot();
+    })
+
+    test("Should render the correct number of ItemCounter components", () => {
+        render(<FirstStepsApp/>);
+        const itemCounters = screen.getAllByTestId("ItemCounter");
+        expect(itemCounters.length).toBe(3);
+
     })
 })
